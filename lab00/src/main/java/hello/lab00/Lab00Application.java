3786812d6d7cb962cc0fcaa1b7ba6593a39805b0
João Antônio Bandeira <joao.martins@ccc.ufcg.edu.br>
2019-08-23 09:44:05 -0300

    Update files.

---
+++ lab00/src/main/java/hello/lab00/Lab00Application.java
@@ -0,0 +1,13 @@
+package hello.lab00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab00Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab00Application.class, args);
+	}
+
+}
